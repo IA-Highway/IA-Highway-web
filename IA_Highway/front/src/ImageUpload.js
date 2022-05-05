c478b1f4e82a862c6cde56150df2b3158c05896a
YasmineMaliki <yasminamaliki@gmail.com>
2022-05-05 14:51:00 +0000

    Position Commit

--- IA_Highway/front/src/ImageUpload.js
+++ IA_Highway/front/src/ImageUpload.js
@@ -1,34 +1,104 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { render } from "react-dom";
 import { storage } from "./firebase";
 import firebase from "firebase"
 
 
 function ImageUpload(){
+
+    const useGeoLocationX = () => {
+        const [locationX, setLocationX] = useState({
+            loaded: false,
+            lat: "",
+        });
+    
+        const onSuccessX = (locationX) => {
+            setLocationX({
+                latitude: locationX.coords.latitude,
+            });
+        };
+    
+        
+        const onErrorX = (error) => {
+            setLocationX({
+                loaded: true,
+                error: {
+                    code: error.code,
+                    message: error.message,
+                },
+            });
+        };
+    
+        useEffect(() => {
+            if (!("geolocation" in navigator)) {
+                onErrorX({
+                    code: 0,
+                    message: "Geolocation not supported",
+                });
+            }
+    
+            navigator.geolocation.getCurrentPosition(onSuccessX, onErrorX);
+        }, []);
+    
+        return locationX;
+    };
+    const useGeoLocationY = () => {
+        const [locationY, setLocationY] = useState({
+            loaded: false,
+            lng: "",
+        });
+        const onSuccessY = (locationY) => {
+            setLocationY({
+                longitude: locationY.coords.latitude,
+            });
+        };
+        
+    
+        const onErrorY = (error) => {
+            setLocationY({
+                loaded: true,
+                error: {
+                    code: error.code,
+                    message: error.message,
+                },
+            });
+        };
+    
+        useEffect(() => {
+            if (!("geolocation" in navigator)) {
+                onErrorY({
+                    code: 0,
+                    message: "Geolocation not supported",
+                });
+            }
+            navigator.geolocation.getCurrentPosition(onSuccessY, onErrorY);
+        }, []);
+    
+        return locationY;
+    };
+    
+    const locationX = useGeoLocationX();
+    const locationY = useGeoLocationY();
     const [image, setImage] = useState(null);
+    const [title, setTitle] = useState(null);
     const [url, setUrl] = useState("");
     const [progress, setProgress] = useState(0);
     const db = firebase.firestore();
     
     const [selectedImage, setSelectedImage] = useState();
 
-    // This function will be triggered when the file field change
-    const imageChange = (e) => {
-      if (e.target.files && e.target.files.length > 0) {
-        setSelectedImage(e.target.files[0]);
-      }
-    }; 
     const handleChange = e => {
     if (e.target.files[0]) {
         setImage(e.target.files[0]);
     }
     if (e.target.files && e.target.files.length > 0) {
         setSelectedImage(e.target.files[0]);
-    } 
+    }
     };
 
     const handleUpload = () => {
     const uploadTask = storage.ref(`images/${image.name}`).put(image);
+
     uploadTask.on(
         "state_changed",
         snapshot => {
@@ -53,6 +123,19 @@ function ImageUpload(){
                     width:0,
                     height:0,
                 });
+                db.collection("gps_location")
+                .add({
+                    longitude: locationY,
+                    latitude: locationX,
+                });
+                db.collection("objects")
+                .add({
+                    description:"hh",
+                    xmax:0,
+                    xmin:0,
+                    ymax:0,
+                    ymin:0,
+                });
             });
         }
     );
@@ -73,7 +156,7 @@ function ImageUpload(){
                 <progress value={progress} max="100" />             
                 <input type="file" onChange={handleChange}  accept="image/*" id="file" style={{ display: 'none' }}/>
                 <label for="file">Prendre Image</label>
-                <input type="text" placeholder='Titre'/>
+                <input type="text" placeholder='Titre' onClick={handleChange} name="title"/>
                 <label onClick={handleUpload}>Upload</label>
             </div>
             </div>  
