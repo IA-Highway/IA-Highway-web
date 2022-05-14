4abfc1b350c6cee4e2fd600457d417ffe08f5a37
Abdelilah <abdelilah.ngadi27@gmail.com>
2022-05-14 16:56:16 +0100

    IA-Highway Draw Polygon With Static Image

--- IA_Highway/front/src/App.js
+++ IA_Highway/front/src/App.js
@@ -1,31 +1,42 @@
-import './App.css';
-import React, { useState, useEffect,useRef } from "react";
+import "./App.css";
+import React, { useState, useEffect, useRef } from "react";
 import { storage } from "./firebase";
-import PhotoCameraOutlinedIcon from '@mui/icons-material/PhotoCameraOutlined';
-import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
-import CropOutlinedIcon from '@mui/icons-material/CropOutlined';
-import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
-import ButtonCard from './components/ButtonCard.jsx'
+import PhotoCameraOutlinedIcon from "@mui/icons-material/PhotoCameraOutlined";
+import FileUploadOutlinedIcon from "@mui/icons-material/FileUploadOutlined";
+import CropOutlinedIcon from "@mui/icons-material/CropOutlined";
+import SaveOutlinedIcon from "@mui/icons-material/SaveOutlined";
+import ButtonCard from "./components/ButtonCard.jsx";
 
-import ImageUpload from './ImageUpload'
+import ImageUpload from "./ImageUpload";
 
-
-const inside1={
-  "icon": <PhotoCameraOutlinedIcon sx={{ color: "rgba(252, 221, 236, 1)" ,fontSize: 40 }}/>,
-  "text":"Prendre une photo"
-}
-const inside2={
-  "icon": <CropOutlinedIcon  sx={{ color: "rgba(252, 221, 236, 1)" ,fontSize: 40 }}/>,
-  "text":"Encadrer l'infrastructure"
-}
-const inside3={
-  "icon": <SaveOutlinedIcon  sx={{ color: "rgba(252, 221, 236, 1)" ,fontSize: 40 }}/>,
-  "text":"Enregistrer en métadonnées"
-}
-const inside4={
-  "icon": <FileUploadOutlinedIcon  sx={{ color: "rgba(252, 221, 236, 1)" ,fontSize: 40 }}/>,
-  "text":"Uploader photo"
-}
+const inside1 = {
+  icon: (
+    <PhotoCameraOutlinedIcon
+      sx={{ color: "rgba(252, 221, 236, 1)", fontSize: 40 }}
+    />
+  ),
+  text: "Prendre une photo",
+};
+const inside2 = {
+  icon: (
+    <CropOutlinedIcon sx={{ color: "rgba(252, 221, 236, 1)", fontSize: 40 }} />
+  ),
+  text: "Encadrer l'infrastructure",
+};
+const inside3 = {
+  icon: (
+    <SaveOutlinedIcon sx={{ color: "rgba(252, 221, 236, 1)", fontSize: 40 }} />
+  ),
+  text: "Enregistrer en métadonnées",
+};
+const inside4 = {
+  icon: (
+    <FileUploadOutlinedIcon
+      sx={{ color: "rgba(252, 221, 236, 1)", fontSize: 40 }}
+    />
+  ),
+  text: "Uploader photo",
+};
 
 function App() {
   const fileInput = React.useRef();
@@ -35,7 +46,7 @@ function App() {
     window.scrollY > 10 ? setnavColor("#252734") : setnavColor("transparent");
     window.scrollY > 10 ? setnavSize("5rem") : setnavSize("10rem");
   };
-  
+
   const [selectedImage, setSelectedImage] = useState();
 
   // This function will be triggered when the file field change
@@ -43,8 +54,8 @@ function App() {
     if (e.target.files && e.target.files.length > 0) {
       setSelectedImage(e.target.files[0]);
     }
-  }; 
-  
+  };
+
   useEffect(() => {
     window.addEventListener("scroll", listenScrollEvent);
     return () => {
@@ -53,73 +64,84 @@ function App() {
   }, []);
 
   const [data, setData] = useState([]);
-  const [image, setImage] = useState('');
+  const [image, setImage] = useState("");
 
-  
   const listItem = () => {
-    storage.ref().child('images/').listAll()
-      .then(res => {
+    storage
+      .ref()
+      .child("images/")
+      .listAll()
+      .then((res) => {
         res.items.forEach((item) => {
           item.getDownloadURL().then((url) => {
-           
-            setData(arr => [...arr, url]);
-           
-          })
-          
-        })
+            setData((arr) => [...arr, url]);
+          });
+        });
       })
-      .catch(err => {
+      .catch((err) => {
         alert(err.message);
-      })
-  }
-  
+      });
+  };
 
-
-   
   return (
-    <div className="App" >
-       <nav class="navbar" id="navbarJs" style={{
+    <div className="App">
+      <nav
+        class="navbar"
+        id="navbarJs"
+        style={{
           backgroundColor: navColor,
-          transition: "all "
-        }}>
-          <div class="logo">IA-HIGHWAY</div>
-          <ul class="nav-links">
-            <input type="checkbox" id="checkbox_toggle" />
-            <label for="checkbox_toggle" class="hamburger">&#9776;</label>
-            <div class="menu">
-              <li>
-                <a href="#">Acceuil</a></li>
-              <li><a href="#fonctionnement">Fonctionnement</a></li>
-              <li><a href="#conteneur">Métadonnée</a></li>
-              <li><a href="#footer">Contact</a></li>
-            </div>
-         </ul>
-        </nav>
-        <div className="eclipse" id="acceuil"></div>
-        <div class="conteneur">
-          <div class="bienvenue">
-            <h1>Bienvenue sur IA-HIGHWAY</h1>
-            <div class="parag">
-              Eviter les dommages
-              <br/>
-              Optimiser/Automatiser les maintenance des infrastructure
-            </div>
+          transition: "all ",
+        }}
+      >
+        <div class="logo">IA-HIGHWAY</div>
+        <ul class="nav-links">
+          <input type="checkbox" id="checkbox_toggle" />
+          <label for="checkbox_toggle" class="hamburger">
+            &#9776;
+          </label>
+          <div class="menu">
+            <li>
+              <a href="#">Acceuil</a>
+            </li>
+            <li>
+              <a href="#fonctionnement">Fonctionnement</a>
+            </li>
+            <li>
+              <a href="#conteneur">Métadonnée</a>
+            </li>
+            <li>
+              <a href="#footer">Contact</a>
+            </li>
           </div>
-        <div className="form4">
-        <div className='form-group'>
-            <div className='imag'> </div>
-            <p id='paragraphe'>Titre 1</p>
-            <hr id='hr'/>
-            <p id='paragraphe'>21/05/2021</p>
+        </ul>
+      </nav>
+      <div className="eclipse" id="acceuil"></div>
+      <div class="conteneur">
+        <div class="bienvenue">
+          <h1>Bienvenue sur IA-HIGHWAY</h1>
+          <div class="parag">
+            Eviter les dommages
+            <br />
+            Optimiser/Automatiser les maintenance des infrastructure
+          </div>
         </div>
+        <div className="form4">
+          <div className="form-group">
+            <div className="imag"> </div>
+            <p id="paragraphe">Titre 1</p>
+            <hr id="hr" />
+            <p id="paragraphe">21/05/2021</p>
+          </div>
         </div>
       </div>
       <div className="eclipse1"></div>
       <div id="fonctionnement">
-        
         <h3>Pour commencer:</h3>
-        <p>Préferable de suivre ces etapes afin de pouvoir assurer la fonctionnalité du traitement</p>
-        <div class="contain"> 
+        <p>
+          Préferable de suivre ces etapes afin de pouvoir assurer la
+          fonctionnalité du traitement
+        </p>
+        <div class="contain">
           <div class="blur"></div>
           <ButtonCard inside={inside1} />
           <ButtonCard inside={inside2} />
@@ -128,73 +150,72 @@ function App() {
         </div>
       </div>
       <div>
-      <ImageUpload/>
+        <ImageUpload />
       </div>
-        
-      
+
       <div id="conteneur">
-        <button className="text" onClick={listItem} > Les Métadonnées</button>
-        <div className='form3'>
-            <div className="choix">
-              <button className="categorie"> Catégorie</button>
-              <button className="recent"> Récent</button>
-              <button className="filtre"> Filtrer</button>
-           </div>
-          </div>     
+        <button className="text" onClick={listItem}>
+          {" "}
+          Les Métadonnées
+        </button>
+        <div className="form3">
+          <div className="choix">
+            <button className="categorie"> Catégorie</button>
+            <button className="recent"> Récent</button>
+            <button className="filtre"> Filtrer</button>
+          </div>
+        </div>
       </div>
       <div className="form2">
-        <br /><br />
-          {data.map((val) => {
-                  return (
-                    <div className='form-group'>
-                      <img className='imag' src={val}/> 
-                      <p id='paragraphe'>Titre 1</p>
-                      <hr id='hr'/>
-                      <p id='paragraphe'>21/05/2021</p>
-                    </div>
-                  );
-              })}
-       
+        <br />
+        <br />
+        {data.map((val) => {
+          return (
+            <div className="form-group">
+              <img className="imag" src={val} />
+              <p id="paragraphe">Titre 1</p>
+              <hr id="hr" />
+              <p id="paragraphe">21/05/2021</p>
+            </div>
+          );
+        })}
       </div>
-       
+
       <footer id="footer">
-      <div class="row primary">
-        <div class="column about">
-          <p>
-          IA-Highway doit permettre à un utilisateur de sélectionner/dessiner une région/polygone entourant un objet d'intérêt, dans ce cas, des images de certaines structures de génie civil qui contiennent des dommages seront fournies
-          </p>
-        </div>
+        <div class="row primary">
+          <div class="column about">
+            <p>
+              IA-Highway doit permettre à un utilisateur de
+              sélectionner/dessiner une région/polygone entourant un objet
+              d'intérêt, dans ce cas, des images de certaines structures de
+              génie civil qui contiennent des dommages seront fournies
+            </p>
+          </div>
 
-        <div class="column links">
-          <h2>Naviguer</h2>
+          <div class="column links">
+            <h2>Naviguer</h2>
             <ul>
               <li>
-              <a href="#">Accueil</a>
+                <a href="#">Accueil</a>
               </li>
               <li>
-              <a href="#fonctionnement">Fonctionnement</a>
+                <a href="#fonctionnement">Fonctionnement</a>
               </li>
               <li>
-              <a href="#conteneur">Métadonnées</a>
+                <a href="#conteneur">Métadonnées</a>
               </li>
             </ul>
-        </div>
-        <div class="column subscribe">
-          <h2>Contact Us</h2>
+          </div>
+          <div class="column subscribe">
+            <h2>Contact Us</h2>
             <ul>
-              <li>
-              donscat24@gmail.com
-              </li>
+              <li>donscat24@gmail.com</li>
             </ul>
+          </div>
         </div>
-      </div>
-    </footer>
-
+      </footer>
     </div>
   );
-
-
-  
 }
 
 export default App;
--- IA_Highway/front/src/ImageUpload.js
+++ IA_Highway/front/src/ImageUpload.js
@@ -1,185 +1,230 @@
-import React, { useState,useEffect,useRef } from "react";
-
+import React, { useState, useEffect, useRef } from "react";
+import Home from './pages/home';
 import { storage } from "./firebase";
 import firebase from "firebase";
-import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile,
+} from "react-device-detect";
 
+function ImageUpload() {
+  const useGeoLocationX = () => {
+    const [locationX, setLocationX] = useState({
+      loaded: false,
+      lat: "",
+    });
 
-
-function ImageUpload(){
-
-    const useGeoLocationX = () => {
-        const [locationX, setLocationX] = useState({
-            loaded: false,
-            lat: "",
-        });
-    
-        const onSuccessX = (locationX) => {
-            setLocationX({
-                latitude: locationX.coords.latitude,
-            });
-        };
-    
-        
-        const onErrorX = (error) => {
-            setLocationX({
-                loaded: true,
-                error: {
-                    code: error.code,
-                    message: error.message,
-                },
-            });
-        };
-    
-        useEffect(() => {
-            if (!("geolocation" in navigator)) {
-                onErrorX({
-                    code: 0,
-                    message: "Geolocation not supported",
-                });
-            }
-    
-            navigator.geolocation.getCurrentPosition(onSuccessX, onErrorX);
-        }, []);
-    
-        return locationX;
+    const onSuccessX = (locationX) => {
+      setLocationX({
+        latitude: locationX.coords.latitude,
+      });
     };
-    const useGeoLocationY = () => {
-        const [locationY, setLocationY] = useState({
-            loaded: false,
-            lng: "",
-        });
-        const onSuccessY = (locationY) => {
-            setLocationY({
-                longitude: locationY.coords.latitude,
-            });
-        };
-        
-    
-        const onErrorY = (error) => {
-            setLocationY({
-                loaded: true,
-                error: {
-                    code: error.code,
-                    message: error.message,
-                },
-            });
-        };
-    
-        useEffect(() => {
-            if (!("geolocation" in navigator)) {
-                onErrorY({
-                    code: 0,
-                    message: "Geolocation not supported",
-                });
-            }
-            navigator.geolocation.getCurrentPosition(onSuccessY, onErrorY);
-        }, []);
-    
-        return locationY;
+
+    const onErrorX = (error) => {
+      setLocationX({
+        loaded: true,
+        error: {
+          code: error.code,
+          message: error.message,
+        },
+      });
     };
-    
-    const locationX = useGeoLocationX().latitude;
-    const locationY = useGeoLocationY().longitude;
-    const [image, setImage] = useState(null);
-    const [title, setTitle] = useState(null);
-    const [url, setUrl] = useState("");
-    const [progress, setProgress] = useState(0);
-    const db = firebase.firestore();
-    
-    const handleTodoChange = (e) => {
-        setTitle(e.target.value);
-    }
-    const [selectedImage, setSelectedImage] = useState();
-    const handleChange = e => {
+
+    useEffect(() => {
+      if (!("geolocation" in navigator)) {
+        onErrorX({
+          code: 0,
+          message: "Geolocation not supported",
+        });
+      }
+
+      navigator.geolocation.getCurrentPosition(onSuccessX, onErrorX);
+    }, []);
+
+    return locationX;
+  };
+  const useGeoLocationY = () => {
+    const [locationY, setLocationY] = useState({
+      loaded: false,
+      lng: "",
+    });
+    const onSuccessY = (locationY) => {
+      setLocationY({
+        longitude: locationY.coords.latitude,
+      });
+    };
+
+    const onErrorY = (error) => {
+      setLocationY({
+        loaded: true,
+        error: {
+          code: error.code,
+          message: error.message,
+        },
+      });
+    };
+
+    useEffect(() => {
+      if (!("geolocation" in navigator)) {
+        onErrorY({
+          code: 0,
+          message: "Geolocation not supported",
+        });
+      }
+      navigator.geolocation.getCurrentPosition(onSuccessY, onErrorY);
+    }, []);
+
+    return locationY;
+  };
+
+  const locationX = useGeoLocationX().latitude;
+  const locationY = useGeoLocationY().longitude;
+  const [image, setImage] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [url, setUrl] = useState("");
+  const [progress, setProgress] = useState(0);
+  const db = firebase.firestore();
+
+  const handleTodoChange = (e) => {
+    setTitle(e.target.value);
+  };
+  const [selectedImage, setSelectedImage] = useState();
+  const handleChange = (e) => {
     if (e.target.files[0]) {
-        setImage(e.target.files[0]);
+      setImage(e.target.files[0]);
     }
     if (e.target.files && e.target.files.length > 0) {
-        setSelectedImage(e.target.files[0]);
+      setSelectedImage(e.target.files[0]);
     }
-    };
-    const ref = useRef(null);
-    const handleUpload = () => {
+  };
+  const ref = useRef(null);
+  const handleUpload = () => {
     const uploadTask = storage.ref(`images/${image.name}`).put(image);
-    let c=0;
+    let c = 0;
 
     uploadTask.on(
-        "state_changed",
-        snapshot => {
+      "state_changed",
+      (snapshot) => {
         const progress = Math.round(
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
         );
         setProgress(progress);
-        },
-        error => {
+      },
+      (error) => {
         console.log(error);
-        },
-        () => {
+      },
+      () => {
         storage
-            .ref("images")
-            .child(image.name)
-            .getDownloadURL()
-            .then((url)=>{
-                firebase.database().ref('images').push({
-                    file_url:url,
-                    date_captured:Date(),
-                    width:ref.current.clientWidth,
-                    height:ref.current.clientHeight,
-                }).child("gps_location")
-                .set({
-                    longitude: locationY,
-                    latitude: locationX,
-                })
-                firebase.database().ref('images').child(image.id+"objects").set({
-                    description:title,
-                    xmax:0,
-                    xmin:0,
-                    ymax:0,
-                    ymin:0,
-                    
-                });
-
-            });
-           
-        }
+          .ref("images")
+          .child(image.name)
+          .getDownloadURL()
+          .then((url) => {
+            firebase
+              .database()
+              .ref("images")
+              .push({
+                file_url: url,
+                date_captured: Date(),
+                width: ref.current.clientWidth,
+                height: ref.current.clientHeight,
+              })
+              .child("gps_location")
+              .set({
+                longitude: locationY,
+                latitude: locationX,
+              });
+            firebase
+              .database()
+              .ref("images")
+              .child(image.id + "objects")
+              .set({
+                description: title,
+                xmax: 0,
+                xmin: 0,
+                ymax: 0,
+                ymin: 0,
+              });
+          });
+      }
     );
-    
-    
-    };
-    
-    console.log("image: ", image);
+  };
 
-    return ( 
+  const showPolygonPage = () => {
+    console.log("Abdel");
+    //Display index.html polygon page
+
+
+
+  };
+
+  console.log("image: ", image);
+
+  return (
     <div>
-         <form className="form">
-            <div className='contImage'>
+      <form className="form">
+        <div className="contImage">
+          <div className="image">
+            {" "}
+            {selectedImage && (
+              <img
+                src={URL.createObjectURL(selectedImage)}
+                id="here"
+                name="image"
+                alt="Thumb"
+                ref={ref}
+              />
+            )}
+          </div>
+        </div>
+        <div className="contButton">
+          <div className="form-group">
+            <progress value={progress} max="100" />
 
-            <div className='image'> {selectedImage && ( <img src={URL.createObjectURL(selectedImage)}id="here" name="image" alt="Thumb"  ref={ref}/> )}
-            </div>
-            </div>
-            <div className='contButton'>
-            <div className="form-group">   
-                <progress value={progress} max="100"/>             
-
-                <BrowserView>
-                <input type="file" onChange={handleChange}  accept="image/*" id="file" style={{ display: 'none' }}/>
-                <label for="file">Selectionner Image</label>
-</BrowserView>
-<MobileView>
-<input type="file" onChange={handleChange}  accept="image/*" id="file" style={{ display: 'none' }}/>
-                <label for="file">Selectionner Image</label>
-                <input type="file" onChange={handleChange}  accept="image/*" id="camera" style={{ display: 'none' }} capture/>
-                <label for="camera">Prendre Image</label>
-</MobileView>
-                <input type="text" placeholder='Titre' value ={title} onChange={handleTodoChange}  name="title"/>
-                <label onClick={handleUpload}>Charger</label>
-                <label> Dessiner Polygone</label>
-            </div>
-            </div>  
-        </form>
+            <BrowserView>
+              <input
+                type="file"
+                onChange={handleChange}
+                accept="image/*"
+                id="file"
+                style={{ display: "none" }}
+              />
+              <label for="file">Selectionner Image</label>
+            </BrowserView>
+            <MobileView>
+              <input
+                type="file"
+                onChange={handleChange}
+                accept="image/*"
+                id="file"
+                style={{ display: "none" }}
+              />
+              <label for="file">Selectionner Image</label>
+              <input
+                type="file"
+                onChange={handleChange}
+                accept="image/*"
+                id="camera"
+                style={{ display: "none" }}
+                capture
+              />
+              <label for="camera">Prendre Image</label>
+            </MobileView>
+            <input
+              type="text"
+              placeholder="Titre"
+              value={title}
+              onChange={handleTodoChange}
+              name="title"
+            />
+            <label onClick={handleUpload}>Charger</label>
+            <label onClick={showPolygonPage}> Dessiner Polygone</label>
+          </div>
+        </div>
+      </form>
     </div>
-    );
-};
+  );
+}
 
-export default ImageUpload
+export default ImageUpload;
